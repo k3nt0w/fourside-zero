40d03302c1532d999a4d0aab56fd535391a73e41
Kento Watanabe <kento.watanabe.11@gmail.com>
2019-07-07 03:56:33 +0900

    feat: support typescript (#3)
    * feat: install next-typescirpt
    
    * chore: prettier config
    
    * chore: add typescript lib
    
    * feat: to be typescript
    
    * chore: fix next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    // Do not run type checking twice:
+    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
     return config
   }
-}
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import Head from '../src/components/head'
+import Nav from '../src/components/nav'
 
 const Home = () => (
   <div>
