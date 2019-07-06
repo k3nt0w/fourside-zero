f5edb532aef675d551319989852ed2fb467411d2
k3nt0w <k3nt0.watanabe@gmail.com>
2019-07-07 03:52:00 +0900

    chore: fix next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withTypescript = require('@zeit/next-typescript');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = withTypescript({
   webpack(config, options) {
     // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
-    return config;
+    return config
   }
-});
+})
